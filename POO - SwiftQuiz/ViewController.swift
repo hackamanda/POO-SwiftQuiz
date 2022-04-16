39cc925afa8f792c1c1568d50a237d2c42f941f7
hackamanda <hckamanda@gmail.com>
2022-04-16 12:42:58 -0300

    Initial Commit

---
+++ POO - SwiftQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  POO - SwiftQuiz
+//
+//  Created by Amanda Hack on 16/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
